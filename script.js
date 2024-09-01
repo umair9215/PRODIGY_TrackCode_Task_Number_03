8163e33fca8b1cd1d392dac63dc992cc2b902c92
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-08-31 19:46:11 -0700

    THIRD TASK DONE

---
+++ script.js
@@ -0,0 +1,14 @@
+const navbar = document.querySelector('.navbar');
+const toggle = document.getElementById('toggle');
+
+toggle.onclick = function(){
+    navbar.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
